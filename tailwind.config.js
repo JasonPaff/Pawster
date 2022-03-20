e6ffab8edfa7a136a8f2b747938fa0e9c4c2765d
Jason <jasonpaff@gmail.com>
2022-03-20 13:35:19 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
