600639fff7716415956d5e4147e7810896f599f1
Jason <jasonpaff@gmail.com>
2022-03-21 15:35:47 -0400

    user created mutation added

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -1,6 +1,8 @@
 const { createApplication } = require('graphql-modules');
 const { userModule } = require('./user/user');
+const {addressModule} = require("./address/address");
+const {dateModule} = require("./date/date");
 
 module.exports.graphql_schema = createApplication({
-    modules: [userModule]
+    modules: [userModule, addressModule, dateModule]
 }).createSchemaForApollo();
