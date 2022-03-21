66f443aba7f57c6a57d735971f98a4ebee64cd23
Jason <jasonpaff@gmail.com>
2022-03-21 14:50:43 -0400

    apollo server + mongodb connection setup

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -1,5 +1,5 @@
 const { createApplication } = require('graphql-modules');
-const { userModule } = require('./user');
+const { userModule } = require('./user/user');
 
 module.exports.application = createApplication({
     modules: [userModule]
