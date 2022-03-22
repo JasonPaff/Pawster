968230789fda6d499e6907b52a91ea55f3078070
Jason <jasonpaff@gmail.com>
2022-03-21 21:19:47 -0400

    front-end registration api route implemented

--- server/mongodb/schemas/user/user.js
+++ server/mongodb/schemas/user/user.js
@@ -3,6 +3,5 @@ const mongoose = require('mongoose');
 module.exports.user_schema = new mongoose.Schema({
     email: String,
     password: String,
-    username: String,
     dateCreated: {type: Date, default: Date.now}
 });
