14faf664d1e86a27922c727865f81b6f838e4fcf
Jason <jasonpaff@gmail.com>
2022-03-21 20:33:13 -0400

    front-end registration api route implemented

--- src/services/authentication/register.js
+++ src/services/authentication/register.js
@@ -1,19 +1,32 @@
-import { useNavigate } from 'react-router-dom'
+import {apiRoute} from "../../utils/apiRoute";
 
-const navigate = useNavigate()
+export default async function createAccount(email, password) {
+    const query = `mutation Mutation($user: UserInput) {
+        createUser(user: $user) {
+            success
+            message
+        }
+    }`
 
-const handleSaveUser = (user) => {
-    fetch('#', {
-        method: 'POST', 
+    // TODO: add jwt to header when implemented
+
+    const headers = {
+        method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
-        }, 
-        body: JSON.stringify(user) 
-    }).then(response => response.json())
-    .then(result => {
-        navigate("/")
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+            variables: {
+                input: {
+                    email,
+                    password
+                }
+            }
+        })
+    };
 
-    })
+    const request = await fetch(`${apiRoute}/graphql`, headers);
+    return await request.json();
 }
-
-export default handleSaveUser
