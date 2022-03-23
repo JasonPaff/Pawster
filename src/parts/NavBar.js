d69b60abefae479152bffb075f5433cf7c81019e
T0lst0v <serghei.tolstov@gmail.com>
2022-03-23 15:34:32 -0400

    pulling

--- src/parts/NavBar.js
+++ src/parts/NavBar.js
@@ -2,15 +2,21 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div class="flex justify-between">
-      <div><h1>Logo goes here</h1></div>
+    <div className="flex justify-between">
+      <div>
+        <h1 className="font-logo text-red-700 text-7xl ml-2">Petsy</h1>
+      </div>
 
       <div>Something Goes Here</div>
 
-      <div class="flex">
-        <div><NavLink to="/login">Login</NavLink></div>
-        <div><NavLink to="/logout">Logout</NavLink></div>           
+      <div className="flex">
+        <div>
+          <NavLink to="/login">Login</NavLink>
+        </div>
+        <div>
+          <NavLink to="/logout">Logout</NavLink>
+        </div>
       </div>
     </div>
   );
-};
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  content: [
-    'src/**/*.js',
-    'src/**/*.jsx',
-    'src/**/*.ts',
-    'src/**/*.tsx',
-    'public/**/*.html',
-  ],
+  content: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx", "public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+    },
+    fontFamily: {
+      body: ["Poppins"],
+      logo: ["Rochester"],
+    },
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
