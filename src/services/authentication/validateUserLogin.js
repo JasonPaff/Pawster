617ed32e2559f5f3fd930872ee835a9df67176ac
Toby <tobywas@gmail.com>
2022-03-24 13:47:20 -0400

    edit and update address

--- src/services/authentication/validateUserLogin.js
+++ src/services/authentication/validateUserLogin.js
@@ -10,6 +10,7 @@ export default async function validateUserLogin(email, password) {
             token
             user {
                 email
+                
             }
         }
     }`
@@ -34,11 +35,11 @@ export default async function validateUserLogin(email, password) {
     const response = await request.json();
 
     if (response.data.validateUserLogin.success) {
-        // console.log(response.data.validateUserLogin.user._id)
+        console.log(response.data.validateUserLogin)
         // TODO: response.data.validateUserLogin.user._id is undefined in the localStorage, was originally user.id (also undefined)
         localStorage.setItem('jsonwebtoken', response.data.validateUserLogin.token);
         localStorage.setItem('email', response.data.validateUserLogin.user.email);
-        localStorage.setItem('userId', response.data.validateUserLogin.user._id);
+        localStorage.setItem('userId', response.data.validateUserLogin.user.id);
     }
 
     return response;
