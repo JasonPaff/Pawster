98da55f1dff4fdcd5efb47ada5923b3003492fa7
Jason <jasonpaff@gmail.com>
2022-03-24 12:05:10 -0400

    host services implemented in client

--- server/graphql/graphql_schema.js
+++ server/graphql/graphql_schema.js
@@ -1,11 +1,13 @@
-const { createApplication } = require('graphql-modules');
-const { userModule } = require('./modules/user_module');
-const {addressModule} = require("./modules/address_module");
-const {dateModule} = require("./modules/date_module");
 const {petModule} = require("./modules/pet_module");
+const {hostModule} = require("./modules/host_module");
+const {dateModule} = require("./modules/date_module");
+const { userModule } = require('./modules/user_module');
+const { createApplication } = require('graphql-modules');
+const {addressModule} = require("./modules/address_module");
 const {petPhotoModule} = require("./modules/pet_photo_module");
 const {userPhotoModule} = require("./modules/user_photo_module");
 
 module.exports.graphql_schema = createApplication({
-    modules: [userModule, addressModule, dateModule, petModule, petPhotoModule, userPhotoModule]
+    modules: [userModule, addressModule, dateModule, petModule,
+        petPhotoModule, userPhotoModule, hostModule]
 }).createSchemaForApollo();
