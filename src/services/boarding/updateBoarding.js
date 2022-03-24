6debc64dbce7e0ce14452c20267ac2035a42febb
Jason <jasonpaff@gmail.com>
2022-03-24 12:38:40 -0400

    boarding services implemented in client

---
+++ src/services/boarding/updateBoarding.js
@@ -0,0 +1,42 @@
+﻿import {apiRoute} from "../../utils/apiRoute";
+import {gql} from "@apollo/client";
+import getGqlString from "../../utils/graphql_utils";
+
+export default async function updateBoarding(userId, updatedBoarding) {
+    let query = gql`mutation Mutation($userId: ID!, $updatedBoarding: BoardingInput!) {
+        updateBoarding(userId: $userId, updatedBoarding: $updatedBoarding) {
+            success
+            message
+            boarding {
+                additionalPetRate
+                bathingGroomingRate
+                dropOffRate
+                holidayRate
+                hourlyRate
+                id
+                pickupRate
+                userId
+            }
+        }
+    }`
+    query = getGqlString(query);
+
+    const headers = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            Authorization: localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            query,
+            variables: {
+                userId,
+                updatedBoarding
+            }
+        })
+    };
+
+    const request = await fetch(`${apiRoute}/graphql`, headers);
+    return await request.json();
+};
