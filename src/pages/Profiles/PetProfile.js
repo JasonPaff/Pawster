8d35330e5f7044d37c9f6b4a81dc1118276b1ece
Toby <tobywas@gmail.com>
2022-03-25 14:39:14 -0400

    delete pet button functional on pet profile page

--- src/pages/Profiles/PetProfile.js
+++ src/pages/Profiles/PetProfile.js
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import getPet from '../../services/pet/getPet'
+import deletePet from '../../services/pet/deletePet'
 
 function PetProfile() {
   const params = useParams()
+  const navigate = useNavigate()
 
   const [pet, setPet] = useState({})
 
   useEffect(() => {
-    console.log(params.petId)
     getPet(params.petId).then((result) => {setPet(result.data.getPet.pet)})
   }, [])
 
-  console.log(pet.name)
+  const handleDeletePet = () => {
+    deletePet(params.petId)
+    navigate('/profile')
+
+  }
 
   return (
-    <div className="flex justify-center">
+    <div className="flex-col justify-center">
       <h1>{pet.name}'s Profile Page</h1>
+      <button onClick={handleDeletePet}>Delete Pet</button>
     </div>
   );
 }
