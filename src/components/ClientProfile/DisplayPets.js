2a1e85e7341104ca70a8a908cc46c1dd0f00a551
T0lst0v <serghei.tolstov@gmail.com>
2022-03-31 01:44:03 -0400

    pull resolved

--- src/components/ClientProfile/DisplayPets.js
+++ src/components/ClientProfile/DisplayPets.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+
 import getPets from "../../services/pet/getPets";
 import { NavLink } from "react-router-dom";
 import PetProfilePhoto from "../PetProfilePhoto";
@@ -15,12 +16,18 @@ function DisplayPets() {
 
   const petList = pets.map((pet, index) => {
     return (
-      <li key={index} className="border-b hover:border-b-slate-300 py-1">
-        <PetProfilePhoto petId={pet.id} />
-        <NavLink to={`/profile/pet-profile/${pet.id}`}>{pet.name}</NavLink>
-        <NavLink className="text-sky-400" to={`/profile/pet-profile/edit/${pet.id}`}>
-          Edit
-        </NavLink>
+      <li key={index} className=" flex gap-1 border-b hover:border-b-slate-300 items-center py-1">
+        <div className="w-6 h-6 bg-slate-300 flex-shrink-0">
+          <PetProfilePhoto petId={pet.id} />
+        </div>
+        <span className=" flex-grow">
+          <NavLink to={`/profile/pet-profile/${pet.id}`}>{pet.name}</NavLink>
+        </span>
+        <span className=" text-right text-xs">
+          <NavLink className="text-gray-400" to={`/profile/pet-profile/edit/${pet.id}`}>
+            Edit
+          </NavLink>
+        </span>
       </li>
     );
   });
