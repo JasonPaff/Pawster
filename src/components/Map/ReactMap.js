d6e7d1465af6f5ad37365c3112656d3272ddcf28
Jason <jasonpaff@gmail.com>
2022-03-31 15:51:24 -0400

    working on markers

--- src/components/Map/ReactMap.js
+++ src/components/Map/ReactMap.js
@@ -1,7 +1,7 @@
 ﻿import Map, {Marker} from "react-map-gl";
 import {connect} from "react-redux";
 import {getGeocode} from "./Geocode";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const mapStateToProps = (state) => {
@@ -13,35 +13,45 @@ const token = "pk.eyJ1Ijoic2xlZXB5Ym9va3dvcm0iLCJhIjoiY2wxZmF2cnE4MDBrajNrcGI0cT
 
 function ReactMap(props) {
     const [loaded, setLoaded] = useState(false);
+    const [markers, setMarkers] = useState([]);
+    let hostAddresses = [{lat: 33.7490, lng:-84.5610312}];
 
-    let lat = 33.7490;
-    let lng = -84.5610312;
-    function load () {
+    async function load() {
         const addresses = props.filteredHosts.map((host) => {
             return `${host.street} , ${host.city}`;
         })
-        if(!addresses) return;
-        getGeocode(addresses[0]).then((result) => {
-            lat = result.lat;
-            lng = result.lng;
-            setLoaded(true);
-        });
-    }
+        if (!addresses) return;
 
-    load();
+        for (let c = 0; c < addresses.length; c++) {
+            const result = await getGeocode(addresses[c]).catch((err) => console.log(err));
+            hostAddresses.push({lat: result.lat, lng: result.lng});
+        }
+        let pins = [];
+        hostAddresses.map((address) => {
+            pins.push(<Marker key={address.lng} longitude={address.lng} latitude={address.lat} color="blue"/>)
+        });
+        await setMarkers(pins);
+
+        if (hostAddresses.length > 0)
+            setLoaded(true);
+    }
+    useEffect(() => {
+        load().catch((err) => console.log(err));
+    }, [])
+
     return (
         <>
             {loaded && <Map
                 initialViewState={{
-                    longitude: lng,
-                    latitude: lat,
+                    longitude: hostAddresses[0].lng,
+                    latitude: hostAddresses[0].lat,
                     zoom: 13
                 }}
-                style={{width: 1200, height: 800}}
+                style={{width: '100%', height: 800}}
                 mapStyle="mapbox://styles/mapbox/streets-v9"
                 mapboxAccessToken={token}
             >
-                <Marker longitude={-84.5610312} latitude={33.7490} color="red"/>
+                {markers}
             </Map>}
         </>
     );
