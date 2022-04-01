905a10f37fdd1a167845a681b59734bee927a35f
Jason <jasonpaff@gmail.com>
2022-04-01 16:36:52 -0400

    updated user is host

--- src/components/Map/ReactMap.js
+++ src/components/Map/ReactMap.js
@@ -11,12 +11,13 @@ const mapStateToProps = (state) => {
 }
 const token = "pk.eyJ1Ijoic2xlZXB5Ym9va3dvcm0iLCJhIjoiY2wxZmF2cnE4MDBrajNrcGI0cTdmOXkxdCJ9.cVgltjTDgAB4Ger4rGjSBA";
 
+let hostAddresses = [];
 function ReactMap(props) {
     const [loaded, setLoaded] = useState(false);
     const [markers, setMarkers] = useState([]);
-    let hostAddresses = [{lat: 33.7490, lng:-84.5610312}];
 
     async function load() {
+        console.log('ehee')
         const addresses = props.filteredHosts.map((host) => {
             return `${host.street} , ${host.city}`;
         })
@@ -27,8 +28,8 @@ function ReactMap(props) {
             hostAddresses.push({lat: result.lat, lng: result.lng});
         }
         let pins = [];
-        hostAddresses.map((address) => {
-            pins.push(<Marker key={address.lng} longitude={address.lng} latitude={address.lat} color="blue"/>)
+        hostAddresses = hostAddresses.map((address) => {
+            return (<Marker key={address.lng} longitude={address.lng} latitude={address.lat} color="blue"/>)
         });
         await setMarkers(pins);
 
@@ -36,12 +37,13 @@ function ReactMap(props) {
             setLoaded(true);
     }
     useEffect(() => {
+        console.log(props.filteredHosts);
         load().catch((err) => console.log(err));
-    }, [])
+    }, [props.filteredHosts])
 
     return (
         <>
-            {loaded && <Map
+            {hostAddresses.length > 0 && <Map
                 initialViewState={{
                     longitude: hostAddresses[0].lng,
                     latitude: hostAddresses[0].lat,
