30e36f0f2ad1a8509d384459b7a9a52a7dc5f70f
Jason <jasonpaff@gmail.com>
2022-04-02 16:17:20 -0400

    deploy integrations

--- src/utils/apiRoute.js
+++ src/utils/apiRoute.js
@@ -1,4 +1,4 @@
 ﻿//export const apiRoute = 'http://localhost:4000';
 //export const apiRouteWs = 'ws://localhost:4000';
-export const apiRoute = 'https://pawster.herokuapp.com/';
-export const apiRouteWs = 'wss://pawster.herokuapp.com/';
+export const apiRoute = 'https://pawster.herokuapp.com';
+export const apiRouteWs = 'wss://pawster.herokuapp.com';
