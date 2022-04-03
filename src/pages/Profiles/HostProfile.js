d2130dd6cdc08729916beaa23727df2035b0a95a
Toby <tobywas@gmail.com>
2022-04-02 20:43:49 -0400

    host profile populated

--- src/pages/Profiles/HostProfile.js
+++ src/pages/Profiles/HostProfile.js
@@ -36,33 +36,38 @@ function HostProfile() {
 
   return (
     <div className=" container mx-auto grid grid-rows-2 grid-flow-col gap-4 p-10 h-full">
-      <div className={`row-span-2`}>
+      <div className={`row-span-1 h-20`}>
         <HostProfilePic hostId={params.userId}/>
-      </div>
-
+        <div className="card row-span-1 flex-row mt-5">
+          <div className="text-2xl font-medium mb-2">Services</div>
+          <DisplayServices hostId={params.userId} />
+        </div>
       {isNotLoggedInUser && (
-        <div className="card row-span-1">
+        <div className="card row-span-1 mt-2">
           <span>
             Contact {user.firstName} {user.lastName}
           </span>
           <SendMessage hostId={params.userId} />
         </div>
       )}
-
-      <div className="card row-span-1 flex-row">
-        <div className="text-2xl font-medium mb-2">Services</div>
-        <DisplayServices hostId={params.userId} />
       </div>
+
+
 
       <div className="card row-start-1 row-end-6 col-span-5 p-10">
         <div className="text-4xl font-medium">
           {user.firstName} {user.lastName}
         </div>
+        <div className="mt-10">
+          <NavLink to={`/book-host/${params.userId}`}><button className="rounded-full w-44">Book a Service</button></NavLink>
+        </div>
 
         <div className="mt-10 p-5">
-          <p className="font-medium text-lg">About Me</p>
+          <p className="font-medium text-2xl">About Me</p>
           <div className="p-2">{host.aboutMe}</div>
         </div>
+
+
         <div className="mt-10 underline font-medium">About the Host's Home</div>
         <div className="flex flex-wrap mt-2 justify-evenly w-4/5">
           {host.doesCat ? <div className="flex justify-center text-slate-500 mr-5"><AiOutlineCheck size="1.3em" className="mr-1"/>Can Host Cats</div> : null}
@@ -78,12 +83,28 @@ function HostProfile() {
           {host.canHostUnspayedFemales ? <div className="flex justify-center text-slate-500 mr-5"><AiOutlineCheck size="1.3em" className="mr-1"/>Can Host Unspayed Females</div> : null}
         </div>
 
-
-        <div className="mt-10">
-          <NavLink to={`/book-host/${params.userId}`}><button className="rounded-full w-60">Book Appointment</button></NavLink>
+        <div className="p-5 mt-10">
+          <p className="font-medium text-lg">Type of Home</p>
+          <div className="p-2">{host.typeOfHome}</div>
         </div>
 
-        <div className="mt-10">
+        <div className="p-5">
+          <p className="font-medium text-lg">Type of Yard</p>
+          <div className="p-2">{host.typeOfYard}</div>
+        </div>
+
+        <div className="p-5">
+          <p className="font-medium text-lg">Days Available</p>
+          <div className="p-2">{host.daysAvailable}</div>
+        </div>
+
+        <div className="p-5">
+          <p className="font-medium text-lg">Cancellation Policy</p>
+          <div className="p-2">{host.cancellationPolicy}</div>
+        </div>
+
+
+        <div className="mt-1">
           {user.firstName}'s Pets
           <DisplayHostPets userId={params.userId} />
         </div>
